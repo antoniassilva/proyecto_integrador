26996518013cc6f273aa1b4241c28772c196179c
antoniassilva <antoniasilva@udesa.edu.a>
2024-11-12 11:54:32 -0300

    rutas de producto

--- myapp/controllers/productController.js
+++ myapp/controllers/productController.js
@@ -2,6 +2,23 @@ const productController = {
     index: function (req, res) {
         return res.render("home");
     },
+    detalle: function (req, res) {
+        return res.render("product"); 
+        
+    },
+    showCrear: function (req, res) {
+        return res.render("productAdd"); 
+    },
+
+    saveCraer: function (req, res) {
+        
+    },
+
+    search: function (req, res) {
+        return res.render("searchResults");  
+    }
+
+
 };
 
 module.exports = productController;
--- myapp/routes/products.js
+++ myapp/routes/products.js
@@ -7,4 +7,16 @@ const indexController = require('../controllers/productController');
 /* GET home page */
 router.get('/', indexController.index);
 
+/* GET details page. */
+router.get('/id/:idProducto', indexController.detalle);
+
+/* GET Creat page. */
+router.get('/crear', indexController.showCrear);
+
+/* POST Creat  page. */
+router.post('/crear', indexController.saveCraer);
+
+/* GET Search page. */
+router.get('/busqueda', indexController.search);
+
 module.exports = router;
