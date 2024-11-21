8982cbb1a4ce0e487fc9061ca104e651fe506a71
mariapaznobile <pnobile@udesa.edu.ar>
2024-11-21 15:03:22 -0300

    terminado users

--- myapp/controllers/userController.js
+++ myapp/controllers/userController.js
@@ -2,6 +2,7 @@ const db = require('../database/models');
 const bcryptjs = require('bcryptjs');
 
 const userController = {
+   
     register: (req, res) => {
         return res.render("register"); 
         
@@ -12,9 +13,58 @@ const userController = {
     loginUser: (req, res) => {
         let form = req.body;
 
+        let filtro = {
+            where:{
+                email: form.email
+            }
+        }
 
-        return res.send(form)
+        db.Users.findOne(filtro)
+        .then((result) => {
+
+            if (result != undefined) {
+
+                let validarClave = bcryptjs.compareSync( form.contraseña, result.contraseña);
+                
+                if (validarClave) {
+                    req.session.user = result.dataValues;
+
+                    
+
+                    return res.redirect("/products");
+                } else {
+                    return res.send("Clave incorrecta");
+                }
+
+            } else {
+                return res.send("No se encontro un usuarios");
+            }
+        }).catch((err) => {
+            return console.log(err);
+            
+        });
+
     },
+    
+    results: (req, res) => {
+        /* let qs = req.query.email; */
+
+        let form = req.body;
+        let pass = bcryptjs.hashSync(form.contraseña, 10)
+
+        form.contraseña = pass;
+       
+        db.Users.create(form)
+        .then((result) => {
+            return res.redirect("/users/login")
+        }).catch((err) => {
+            return console.log(err);
+        });
+    },
+    logout: (req, res) => {
+        req.session.destroy()
+        return res.redirect("/products/");
+    }
 };
 
 module.exports = userController;
--- myapp/routes/users.js
+++ myapp/routes/users.js
@@ -4,12 +4,17 @@ var router = express.Router();
 /* Requerir el controlador */
 const userController = require('../controllers/userController');
 
-router.get('/', userController.register);
+
+router.get('/register', userController.register);
+router.post('/register', userController.results);
 
 router.get('/login', userController.login);
 
 /* procesar sufijos Login*/
 router.post('/login', userController.loginUser)
+
+
+router.post("/logout", userController.logout)
 
 
 
